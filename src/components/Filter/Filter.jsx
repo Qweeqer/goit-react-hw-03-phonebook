a8e31ea2a1f68a5abd7c10a22c91fa79757ed20c
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-18 15:27:45 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
+import './Filter.module.css';
+
 export const Filter = ({ filter, changeFilterInput }) => (
   <label>
     <input
       type="text"
       name={filter}
       onChange={changeFilterInput}
-      placeholder="Find contacts by name"
+      placeholder="Find contacts by name or number"
     />
   </label>
 );
